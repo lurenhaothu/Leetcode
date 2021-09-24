ecdb30da7edc582a6e94e772694a8fe13af00765
lurenhaothu <38303642+lurenhaothu@users.noreply.github.com>
2021-09-24 10:09:23 -0400

    Create LC0027.cpp

---
+++ LC0027.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int cur = 0, n = nums.size();
+        for(int i = 0; i < n; i++){
+            if(nums[i] != val){
+                nums[cur] = nums[i];
+                cur++;
+            }
+        }
+        return cur;
+    }
+};
