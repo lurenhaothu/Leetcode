0c1131b01a2a414ecdd3cded05b06010078aaa89
lurenhaothu <38303642+lurenhaothu@users.noreply.github.com>
2021-10-05 13:03:01 -0400

    Create LC0075.cpp

---
+++ LC0075.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int p0 = 0, p2 = nums.size() - 1, p = 0;
+        for(p = 0; p <= p2;){
+            if(nums[p] == 0){
+                nums[p] = nums[p0];
+                nums[p0] = 0;
+                p0++;
+                p++;
+            }else if(nums[p] == 2){
+                nums[p] = nums[p2];
+                nums[p2] = 2;
+                p2--;
+            }else{
+                p++;
+            }
+        }
+        return;
+    }
+};
