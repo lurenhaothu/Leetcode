6f27e7d2f3b58f0de5caba3bf47a5e0a8f298e13
lurenhaothu <38303642+lurenhaothu@users.noreply.github.com>
2021-10-13 21:35:08 -0400

    Update and rename LC0106.cpp to LC0105.cpp

--- LC0105.cpp
+++ LC0105.cpp
@@ -9,6 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+//This problem can also be solve with stack
 class Solution {
 public:
     vector<int> p, i;
